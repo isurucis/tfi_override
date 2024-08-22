4a6410608c84271d5bed926e42c54df2f4cd0300
isurucis <143800833+isurucis@users.noreply.github.com>
2024-08-23 04:55:35 +0530

    Update Cart.php
    get boxing details

--- classes/Cart.php
+++ classes/Cart.php
@@ -477,8 +477,14 @@ class Cart extends CartCore
         bool $keepOrderPrices = false
     ) {
         if ($id_carrier === 8) {//FedEx Standard Overnight
-            $total_shipping_cost = $this->getFedExShippingCost();
+            /*$total_shipping_cost = $this->getFedExShippingCost();*/
+            $items = [
+            ["id" => 0, "ponumber" => 0, "ciscode" => "108033", "sku" => 0, "description" => null, "qty" => 20, "UOM" => null, "individualPacked" => 0, "shipp_withCups" => 0, "cust_type_id" => 4, "Low_temperature" => 0, "High_temperature" => 0],
+            ["id" => 0, "ponumber" => 0, "ciscode" => "120557", "sku" => 0, "description" => null, "qty" => 20, "UOM" => null, "individualPacked" => 0, "shipp_withCups" => 0, "cust_type_id" => 4, "Low_temperature" => 0, "High_temperature" => 0],
+            ];
 
+            $boxingDetails = $this->getBoxingDetails($items);
+            $total_shipping_cost =20;
             return $total_shipping_cost;
         } else {
             if ($this->isVirtualCart()) {
